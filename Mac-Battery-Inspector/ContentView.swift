61a56d29f56cc35261cc123d311c4797159821fd
Ricky <RickyChuang.work@gmail.com>
2023-05-18 14:33:53 +0800

    Initial Commit

---
+++ Mac-Battery-Inspector/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mac-Battery-Inspector
+//
+//  Created by 00592272 on 2023/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mac-Battery-Inspector/Mac_Battery_InspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Battery_InspectorApp.swift
+//  Mac-Battery-Inspector
+//
+//  Created by 00592272 on 2023/5/18.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Battery_InspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
